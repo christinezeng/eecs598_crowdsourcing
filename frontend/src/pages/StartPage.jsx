4718f68a99e4c62fd323443021c61e3ad6397524
cczeng <cczeng@umich.edu>
2025-02-04 22:38:33 -0500

    modified instructions

--- frontend/src/pages/StartPage.jsx
+++ frontend/src/pages/StartPage.jsx
@@ -105,7 +105,7 @@ const StartPage = () => {
 						</Typography>
 						<Typography variant="body1">
 							In the following task, you will be given <strong>200 images</strong> to label as either
-							AI-generated or Real. <strong>Please label all 200 images in one go.</strong> 
+							AI-generated or Real. <strong>Please label all 200 images in one go. </strong> 
 							After you have finished, you will receive a code to paste into Amazon MTurk survey to receive credit for doing this task.
 						</Typography>
 						<Button
